16789d4a37047569f6e04adfd22dc7b2d3efa739
Nicholaos Bilalis <n.bilalis@sae.edu>
2021-07-17 14:13:58 +0300

    Add form inputs for `firstname` & `lastname`

--- app/forms.py
+++ app/forms.py
@@ -13,4 +13,6 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', description='Username', validators=[DataRequired(), Length(min=4, max=16)])
     password = PasswordField('Password', description='Password', validators=[DataRequired(), Length(min=8, max=32)])
     email = StringField('E-Mail', description='E-Mail', validators=[DataRequired(), Email()])
+    firstname = StringField('Firstname', description='Firstname', validators=[DataRequired()])
+    lastname = StringField('Lastname', description='Lastname', validators=[DataRequired()])
     submit_register = SubmitField('Register')
