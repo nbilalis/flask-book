d085dd29dd5ae2ea4161cfe6f1e587074134abd4
Nicholaos Bilalis <n.bilalis@sae.edu>
2021-07-19 18:47:26 +0300

    Add ``UserMIxin`` as base for ``User``

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@
 
 from . import db        # from app import db
 
+from flask_login import UserMixin
+
 from datetime import datetime
 from sqlalchemy import event
 from sqlalchemy.sql import func
@@ -24,7 +26,7 @@ friendships = db.Table(
 )
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
